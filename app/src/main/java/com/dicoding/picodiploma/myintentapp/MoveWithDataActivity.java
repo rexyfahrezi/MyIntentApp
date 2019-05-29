7b5a877b0b078ebf713a44df5645f1af9b64e531
rexyfahrezi <rexyfahrezi30@gmail.com>
2019-05-29 07:17:35 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/picodiploma/myintentapp/MoveWithDataActivity.java
@@ -0,0 +1,28 @@
+    package com.dicoding.picodiploma.myintentapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+    public class MoveWithDataActivity extends AppCompatActivity {
+    public static final String EXTRA_AGE = "extra_age";
+    public static final String EXTRA_NAME = "extra_name";
+
+    TextView tvDataReceived;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_with_data);
+
+        tvDataReceived = findViewById(R.id.tv_data_received);
+
+        String name = getIntent().getStringExtra(EXTRA_NAME);
+        int age = getIntent().getIntExtra(EXTRA_AGE, 0);
+
+        String text = "Name : " + name + ",\nYour Age : " + age;
+        tvDataReceived.setText(text);
+    }
+}
